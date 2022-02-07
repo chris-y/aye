cfef967c1d4be71e4bd58cfddad6fa294b5636ac
Chris Young <chris@unsatisfactorysoftware.co.uk>
2022-02-07 16:42:59 +0000

    Add header offsets to debug

--- zxay_dump.c
+++ zxay_dump.c
@@ -10,10 +10,17 @@
 
 #include "zxay_dump.h"
 #include "zxay_file.h"
+#include "zxay_misc.h"
 
 static uint32_t offset = 0;
 
-static void dump(uint8_t *structure, size_t size)
+enum {
+	ZXAY_S_NONE = 0,
+	ZXAY_S_HEADER
+};
+
+
+static void dump(uint8_t *structure, size_t size, int type)
 {
 	int i = 0;
 
@@ -21,6 +28,15 @@ static void dump(uint8_t *structure, size_t size)
 
 	for(i = 0; i < size; i++) {
 		printf("%02x ", structure[i]);
+
+		switch(type) {
+			case ZXAY_S_HEADER:
+				if((i == (PAUTHOR_OFFSET + 1)) || (i == (PMISC_OFFSET + 1)) || (i == (PSONGSSTRUCTURE_OFFSET + 1))) {
+					printf("{%04x} ", zxay_read_int16(structure + i - 1) + (offset + i - 1));
+				}
+			break;
+		}
+
 	}
 	
 	printf("\n");
@@ -35,26 +51,26 @@ void zxay_dump(void *zxayemul, int level)
 	char *blk = NULL;
 	
 	printf("Header: ");
-	dump((char *)zxay->header, sizeof(struct zxay_header));
+	dump((char *)zxay->header, sizeof(struct zxay_header), ZXAY_S_HEADER);
 	
 	for(i = 0; i <= zxay->header->NumOfSongs; i++) {
 		printf("Track %d:\n", i);
 		printf("  Song struct: ");
-		dump((char *)(zxay->songsstruct + i), sizeof(struct zxay_song));
+		dump((char *)(zxay->songsstruct + i), sizeof(struct zxay_song), 0);
 		printf("  Song data: ");
-		dump((char *)zxay->songdata[i], sizeof(struct zxay_songdata));
+		dump((char *)zxay->songdata[i], sizeof(struct zxay_songdata), 0);
 		printf("  Song ptrs: ");
-		dump((char *)zxay->songptrs[i], sizeof(struct zxay_songptrs));
+		dump((char *)zxay->songptrs[i], sizeof(struct zxay_songptrs), 0);
 		printf("  Song blks:\n");
 		blk = (char *)zxay->songblks[i];
 		for(b = 0; b < (zxay->songblkcount[i]); b++) {
 			printf("    Block %d: ", b);
-			dump(blk, sizeof(struct zxay_songblks));
+			dump(blk, sizeof(struct zxay_songblks), 0);
 			blk += sizeof(struct zxay_songblks);
 		}
 		if(level > 1) {
 			printf("    All block data: \n", b);
-			dump((char *)zxay->datablks[i], zxay->datablk_size[i]);
+			dump((char *)zxay->datablks[i], zxay->datablk_size[i], 0);
 		} else {
 			offset += zxay->datablk_size[i];
 		}
