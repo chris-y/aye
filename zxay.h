a24d8da635a6cebcad2346bdac5068e29f54bbfc
Chris Young <chris@unsatisfactorysoftware.co.uk>
2022-03-08 16:07:18 +0000

    Explicit 8-bit types

--- zxay.h
+++ zxay.h
@@ -8,59 +8,61 @@
 #ifndef ZXAY_H
 #define ZXAY_H 1
 
+#include <stdint.h>
+
 #define PAUTHOR_OFFSET         12
 #define PMISC_OFFSET           14
 #define PSONGSSTRUCTURE_OFFSET 18
 
 struct zxay_header {
-	char FileID[4];
-	char TypeID[4];
-	char FileVersion;
-	char PlayerVersion;
-	char PSpecialPlayer[2];
-	char PAuthor[2];
-	char PMisc[2];
-	char NumOfSongs;
-	char FirstSong;
-	char PSongsStructure[2];
+	uint8_t FileID[4];
+	uint8_t TypeID[4];
+	uint8_t FileVersion;
+	uint8_t PlayerVersion;
+	uint8_t PSpecialPlayer[2];
+	uint8_t PAuthor[2];
+	uint8_t PMisc[2];
+	uint8_t NumOfSongs;
+	uint8_t FirstSong;
+	uint8_t PSongsStructure[2];
 };
 
 #define PSONGNAME_OFFSET 0
 #define PSONGDATA_OFFSET 2
 
 struct zxay_song {
-	char PSongName[2];
-	char PSongData[2];
+	uint8_t PSongName[2];
+	uint8_t PSongData[2];
 };
 
 #define PPOINTS_OFFSET 10
 #define PADDRESSES_OFFSET 12
 
 struct zxay_songdata {
-	char AChan;
-	char BChan;
-	char CChan;
-	char Noise;
-	char SongLength[2];
-	char FadeLength[2];
-	char HiReg;
-	char LoReg;
-	char PPoints[2];
-	char PAddresses[2];
+	uint8_t AChan;
+	uint8_t BChan;
+	uint8_t CChan;
+	uint8_t Noise;
+	uint8_t SongLength[2];
+	uint8_t FadeLength[2];
+	uint8_t HiReg;
+	uint8_t LoReg;
+	uint8_t PPoints[2];
+	uint8_t PAddresses[2];
 };
 
 struct zxay_songptrs {
-	char Stack[2];
-	char INIT[2];
-	char INTERRUPT[2];
+	uint8_t Stack[2];
+	uint8_t INIT[2];
+	uint8_t INTERRUPT[2];
 };
 
 #define OFFSET_OFFSET 4
 
 struct zxay_songblks {
-	char Address[2];
-	char Length[2];
-	char Offset[2];
+	uint8_t Address[2];
+	uint8_t Length[2];
+	uint8_t Offset[2];
 };
 
 #endif
